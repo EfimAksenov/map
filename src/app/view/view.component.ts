4a2ecdb70dff04ad09430fec19f4dccb39a4e251
Efim Aksenov <Efimke54@gmail.com>
2017-12-01 14:15:30 +0700

    реализовал компонент view

--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {RoutesService} from "../services/routes.service";
+import {Entity} from "../interfaces/entity";
+import {CustomRoute} from "../interfaces/custom-route";
 
 @Component({
   selector: 'app-view',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewComponent implements OnInit {
 
-  constructor() { }
+  @Input("lat") initLat = 54.962656;
+  @Input("lng") initLng = 82.932969;
+  @Input("zoom") initZoom = 15;
 
-  ngOnInit() {
+  cities: Entity[];
+  routes: CustomRoute[];
+  selectedCity: Entity;
+  route: CustomRoute;
+
+  timeout = 1000;
+
+  constructor(private routesService: RoutesService) {
+    this.route = routesService.makeRouteInstance();
   }
 
+  ngOnInit() {
+    this.routesService.getCities().subscribe(cities => {
+      this.cities = cities;
+    });
+
+  }
+
+  selectCity(city: Entity) {
+    this.selectedCity = city;
+    this.route.cityId = city.uuid;
+    this.loadRoutesList(this.selectedCity);
+  }
+
+  loadRoutesList(city: Entity) {
+    setTimeout(() => {
+      this.routesService.getRoutes(city).subscribe(routes => {
+        this.routes = routes;
+      });
+    }, this.timeout);
+  }
+
+  loadRoute(route: Entity) {
+    this.routesService.getRoute(route).subscribe(loadedRoute => {
+      this.route = loadedRoute;
+    });
+  }
 }
